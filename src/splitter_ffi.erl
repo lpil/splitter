e2e60fcbd332bb998a117368920d03334d89230f
deadmarshal <adeadmarshal@gmail.com>
2026-08-23 19:57:24 +0330

    Added typespecs

--- src/splitter_ffi.erl
+++ src/splitter_ffi.erl
@@ -1,11 +1,18 @@
 -module(splitter_ffi).
 -export([new/1, split/2, split_before/2, split_after/2, would_split/2]).
 
+-spec new(Patterns) -> R when
+    Patterns :: [binary()],
+    R :: binary:cp().
 new([]) ->
     empty_splitter;
 new(Patterns) ->
     binary:compile_pattern(Patterns).
 
+-spec split(Splitter,String) -> R when
+    Splitter :: empty_splitter | binary:cp(),
+    String :: binary(),
+    R :: {binary(),binary(),binary()}.
 split(empty_splitter, String) ->
     {<<>>, <<>>, String};
 split(Splitter, String) ->
@@ -17,6 +24,10 @@ split(Splitter, String) ->
              binary:part(String, Index + Length, byte_size(String) - Index - Length)}
     end.
 
+-spec split_before(Splitter,String) -> R when
+    Splitter :: empty_splitter | binary:cp(),
+    String :: binary(),
+    R :: {binary(),binary()}.
 split_before(empty_splitter, String) ->
     {<<>>, String};
 split_before(Splitter, String) ->
@@ -27,6 +38,10 @@ split_before(Splitter, String) ->
              binary:part(String, Index, byte_size(String) - Index)}
     end.
 
+-spec split_after(Splitter,String) -> R when
+    Splitter :: empty_splitter | binary:cp(),
+    String :: binary(),
+    R :: {binary(),binary()}.
 split_after(empty_splitter, String) ->
     {<<>>, String};
 split_after(Splitter, String) ->
@@ -38,6 +53,10 @@ split_after(Splitter, String) ->
              binary:part(String, SplitPoint, byte_size(String) - SplitPoint)}
     end.
 
+-spec would_split(Splitter,String) -> R when
+    Splitter :: empty | binary:cp(),
+    String :: binary(),
+    R :: {binary(),binary()}.
 would_split(empty_splitter, _String) ->
     false;
 would_split(Splitter, String) ->
